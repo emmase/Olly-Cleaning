84136bfcdda86775f3856d412ca37aec867cacbd
SSEGONA EMMANUEL <sseggonaemmanuel@gmail.com>
2023-06-30 07:12:01 +0300

    20th commit

--- Script.js
+++ Script.js
@@ -32,3 +32,18 @@ s.onload = function() {
 };
 var x = document.getElementsByTagName('script')[0];
 x.parentNode.insertBefore(s, x);
+
+const bar =document.getElementById('bar');
+const nav =document.getElementById('navbar');
+const close =document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click',() => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click',() => {
+        nav.classList.remove('active');
+    })
+}
